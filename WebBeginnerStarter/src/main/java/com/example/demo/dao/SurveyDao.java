a26d56ddf6c2d3c533423043c25057ea59445e0b
Marimo-R <marimo.r.925@gmail.com>
2023-04-15 21:21:17 +0900

    4/15 Finish, SurveyRegistration, IndexPage Done

--- WebBeginnerStarter/src/main/java/com/example/demo/dao/SurveyDao.java
+++ WebBeginnerStarter/src/main/java/com/example/demo/dao/SurveyDao.java
@@ -1,15 +1,13 @@
-package com.example.demo.repository;
+package com.example.demo.dao;
 
 import java.util.List;
-
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Survey;
 
 public interface SurveyDao {
 
 	void insertSurvey(Survey survey);
-
+	
 	List<Survey> getAll();
-
+	
 }
--- WebBeginnerStarter/src/main/java/com/example/demo/service/SurveyServiceImpl.java
+++ WebBeginnerStarter/src/main/java/com/example/demo/service/SurveyServiceImpl.java
@@ -2,23 +2,25 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.SurveyDao;
 import com.example.demo.entity.Survey;
-import com.example.demo.repository.SurveyDao;
+
 
 @Service
 public class SurveyServiceImpl implements SurveyService {
 
 	private final SurveyDao dao;
 
-	SurveyServiceImpl(SurveyDao dao){
+	@Autowired SurveyServiceImpl(SurveyDao dao){
 		this.dao = dao;
 	}
 
 	@Override
 	public void save(Survey survey) {
-		//hands-on
+		dao.insertSurvey(survey);
 	}
 
 	@Override
